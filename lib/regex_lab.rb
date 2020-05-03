9ab67939f3c12897e32c040a24964846ecea243d
Dina Kendle <dina.kendle@gmail.com>
2020-05-03 16:42:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,14 @@
 require "pry"
-
 def starts_with_a_vowel?(word)
-  if word.match(/^[AEIOU]^[aeiou]$/)
-    true 
-  else 
-    false
-  end
+word.match(/\A[aeiouAEIOU]/) != nil
 end
+# def starts_with_a_vowel?(word)
+#   if word.match(/^[AEIOU]^[aeiou]$/)
+#     true 
+#   else 
+#     false
+#   end
+# end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w+ing/)
